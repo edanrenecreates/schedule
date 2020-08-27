43a19bb740e9ee5663864c4be19d364514264f67
edanrenecreates <rene@edanramos.com>
2020-08-27 13:56:03 -0600

    added icon components

--- src/components/library/libraryCourse.js
+++ src/components/library/libraryCourse.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
+import Icon from '../icon';
 
 class LibraryCourse extends Component {
   render() {
     return (
       <div className="library-course">
         <label className="library-course__title">Problem Solving</label>
-        {/* Icon Component */}
+        {Icon('fas fa-check', 'library-course__icon')}
         {/* Arrow Component */}
-        {/* Action Button */}
+        {/* Action button component */}
         <div className="library-course__description">
           <label>Course Description</label>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas ipsum urna, lacinia et diam a, semper auctor nunc. Sed tellus sem, semper et lacus mollis. </p>
