4c28198466f49651d4ccdcce9db811385f2a3361
edanrenecreates <rene@edanramos.com>
2020-08-30 18:09:34 -0600

    built courses reducer and connected library comp to redux

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
+import rootReducer from './coursesReducer';
 
-const rootReducer = combineReducers({
-  state: (state = {}) => state
+const RootReducer = combineReducers({
+
 });
 
 export default rootReducer;
